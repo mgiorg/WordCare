d15302c9d1f493c2325e30bde3cd81118c772d06
mgiorg <massimo.giord@gmail.com>
2025-05-11 16:34:49 +0200

    Piccoli fix

---
+++ server/database/Query.sql
@@ -0,0 +1,9 @@
+-- SQLite
+Insert into Appuntamento values(1, 1, 1, "2025-06-09", "16:00:00", "Via del Tutto Eccezionale, 15");
+Insert into Appuntamento values(2, 2, 2, "2025-07-15", "17:15:00", "Via del Tutto Eccezionale, 15");
+Insert into Appuntamento values(3, 3, 2, "2025-06-22", "18:30:00", "Via del Tutto Eccezionale, 15");
+
+Select * From User;
+Select * From Paziente;
+Select * From Professionista;
+Select * From Appuntamento;
